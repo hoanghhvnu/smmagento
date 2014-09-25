ffa7ec5e8718ef1e3f96220b8799a0870f5ec40d
hoanghh <hoanghh.vnu@gmail.com>
2014-09-25 15:23:39 +0700

    set mytemplate by observer, custom filter to 4 type ok

--- app/code/local/SM/Filter/Model/Observer/FilterObserver.php
+++ app/code/local/SM/Filter/Model/Observer/FilterObserver.php
@@ -5,7 +5,7 @@
  * Date: 9/25/14
  * Time: 9:35 AM
  */
-class SM_Filter_Model_Observer_AddFieldToEditAttribute
+class SM_Filter_Model_Observer_FilterObserver
 {
     public function addField(Varien_Event_Observer $observer)
     {
@@ -22,5 +22,21 @@ class SM_Filter_Model_Observer_AddFieldToEditAttribute
              )
         );
     } // end method addField()
+
+    public function changeFilterTemplate(Varien_Event_Observer $observer)
+    {
+        $block = $observer->getBlock();
+        if ($block instanceof Mage_Catalog_Block_Layer_View) {
+            $listChild = $block->getChild();
+            foreach ($listChild as $child) {
+                if  ($child->getType() == 'catalog/layer_filter_attribute'
+                    || $child->getType() == 'catalog/layer_filter_price'
+                ) {
+                    $child->setTemplate('catalog/layer/otherfilter.phtml');
+                }
+
+            }
+        } // end if
+    } // end method changeFilterTemplate()
 } // end class
 // end file
--- app/code/local/SM/Filter/Model/Source/FilterRendererType.php
+++ app/code/local/SM/Filter/Model/Source/FilterRendererType.php
@@ -7,13 +7,21 @@
  */
 class SM_Filter_Model_Source_FilterRendererType
 {
+    const DEFAULT_TYPE  = 0;
+    const CHECKBOX_TYPE = 1;
+    const LINK_TYPE     = 2;
+    const SELECT_TYPE   = 3;
+    const COLOR_TYPE    = 4;
+
+
     public function toOptionArray()
     {
         return array(
-            array('value' => 1, 'label' => 'Checkbox'),
-            array('value' => 2, 'label' => 'Link'),
-            array('value' => 3, 'label' => 'Select'),
-            array('value' => 4, 'label' => 'Color'),
+            array('value' => self::DEFAULT_TYPE, 'label' => 'Default'),
+            array('value' => self::CHECKBOX_TYPE, 'label' => 'Checkbox'),
+            array('value' => self::LINK_TYPE, 'label' => 'Link'),
+            array('value' => self::SELECT_TYPE, 'label' => 'Select'),
+            array('value' => self::COLOR_TYPE, 'label' => 'Color'),
         );
     }
 } // end class
